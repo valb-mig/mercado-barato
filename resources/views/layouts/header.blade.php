cae44e1eb73fcc4ac4cb8ee0a6c0a4cfad762154
valb-mig <valb.mig@gmail.com>
2024-03-10 21:22:14 -0300

    :lipstick:(style): Atualizando componentes

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,26 @@
+@php 
+    $user  = \App\Helpers\UserHelper::getUserData();
+    $image = \App\Helpers\UserHelper::getUserImage($user->id)
+@endphp
+
+<nav class="p-2 flex gap-2 items-center border-b-[1px] border-light-1">
+    <div class="flex w-full">
+        <a class="navbar-brand" href="/home">Mercado Barato</a>
+    </div>
+
+    <div class="flex gap-2 items-center">
+        <div class="flex items-center">
+            @include('components.config.icon', [
+                'icon'  => 'bell',
+                'width' => '1em'
+            ])
+        </div>
+        <a href="/user/profile/{{$user->id}}" class="flex items-center text-dark-0 bg-light-1 p-1 rounded no-underline">
+            <p class="mr-1 mb-0">{{$user->username}}</p>
+            <span class="flex w-[2rem] bg-light-2 p-1 rounded">
+                <img src="{{$image->arquivo_base64}}" width="100%" />
+            </span>
+        </a>
+
+    </div>
+</nav>
